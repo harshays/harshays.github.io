8edb01264955012b254c883e5f299ac662de0dff
Harshay <harshay.rshah@gmail.com>
2015-06-21 22:39:04 -0500

    website rewrite. redesigned from scratch.

---
+++ app.js
@@ -0,0 +1,64 @@
+function get(url) {
+    return new Promise(function(success_cb, error_cb) {
+        var http = new XMLHttpRequest();
+        http.open('GET', url)
+
+        http.addEventListener('load', function() {
+            if (http.status < 400) {
+                success_cb(http.responseText);
+            }
+            else {
+                error_cb(new Error(http.status));
+            }
+        })
+
+        http.addEventListener('error', function() {
+            error_cb(new Error());
+        });
+
+        http.send();
+    });
+}
+
+var project_template = "\
+        <div class='col-md-3'> \
+            <a href='{{url}}'>{{name}}</a> \
+        </div> \
+        <div class='col-md-8'> \
+            <p>{{description}}</p> \
+        </div> \ ";
+
+
+var filter = ['harshays.gitub.io', 'website', 
+              'hh-personal-sites', 'learn',
+              'solutions', 'scripts', 'harshays.github.io'];
+
+function append(node, repo) {
+    if (filter.indexOf(repo['name']) > -1) {
+        return;
+    }
+    var info = {
+        'name': repo['name'],
+        'url' : repo['homepage'] || repo['html_url'],
+        'description' : repo['description']
+    };
+    var project_div  = document.createElement('div');
+    project_div.className = 'row project-row';
+    var innerhtml = Mustache.to_html(project_template, info);
+    project_div.innerHTML = innerhtml;
+    node.appendChild(project_div);
+}
+
+window.onload = function() {
+    var repo_url = 'https://api.github.com/users/harshays/repos?sort=pushed';
+    var projects_div = document.querySelector('.projects');
+
+    get(repo_url).then(function(data) {
+        data = JSON.parse(data);
+        data.forEach(function(repo) {
+            append(projects_div, repo);
+        });
+    }, function(err) {
+        console.error(err);
+    });
+};
