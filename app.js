df9735cdbe987fb39b132bcdcb5ea26a7664aae2
Harshay <harshay.rshah@gmail.com>
2015-08-20 00:20:27 -0500

    Remove JQuery, Add Angular JS

--- app.js
+++ app.js
@@ -1,117 +1,65 @@
-/*
-* Promise to get github repo data
-*/
-function get(url) {
-    return new Promise(function(success_cb, error_cb) {
-        var http = new XMLHttpRequest();
-        http.open('GET', url)
-        http.addEventListener('load', function() {
-            if (http.status < 400)
-                success_cb(http.responseText);
-            else
-                error_cb(new Error(http.status));
-        });
-        http.addEventListener('error', function() {
-            error_cb(new Error());
-        });
-        http.send();
-    });
-}
+websiteApp = angular.module('websiteApp', ['ngAnimate']);
 
-/*
-* div for description of each project
-*/
-var project_template = "\
-        <div class='col-md-3'> \
-            <a href='{{url}}'>{{name}}</a> \
-        </div> \
-        <div class='col-md-9'> \
-            <p>{{description}}</p> \
-        </div> \ ";
+websiteApp.config(function($httpProvider) {
+    $httpProvider.defaults.cache = true;
+});
 
-
-// do not show these repositories
-var filter = ['harshays.gitub.io', 'website',
-              'hh-personal-sites', 'learn',
-              'solutions', 'scripts', 'harshays.github.io',
-              'dotfiles'
-              ];
-
-// only show these forked repositories
-var fork_filter = ['phrase-mining']
-
-// filter repo
-function filterRepos(repos) {
-    return repos.filter(function(repo) {
-        var inFilter = filter.indexOf(repo['name']) > -1;
-        var inFork = fork_filter.indexOf(repo['name']) > -1;
-        var isFork = repo['fork'];
-
-        if (isFork) {
-            if (inFork)
-                return true;
-            return false;
-        }
-
-        if (inFilter)
-            return false;
-
-        return true;
-    });
-}
-
-// extract repo key-vals
-function extractRepo(repo) {
-    return  { 'name': repo['name'],
-              'url' : repo['homepage'] || repo['html_url'],
-              'description' : repo['description'] 
-            };
-}
-
-// add repo to dom
-function addRepo(info) {
-    var $repo = $('<div>', {'class': 'row project_row'});
-    $repo.html(Mustache.to_html(project_template, info));
-    $('.projects').append($repo);
-}
-
-// setup before promise handling
-function setup_before() {
-    var window_ht = $(window).height();
-    $('.body_container').height(window_ht);
-}
-
-// setup after promise handling
-function setup_after() {
-    $('body').fadeIn('fast');
-}
-
-$(document).ready(function() {
-    var repo_url = 'https://api.github.com/users/harshays/repos?sort=pushed';
-    var gh_err = "Most of my projects are on <a href='https://www.github.com/harshays'>Github</a>"
-    var $node = $('.gh_change');
-
-    setup_before();
-
-    try {
-        get(repo_url).then(
-        function(data) {
-            var repos = filterRepos(JSON.parse(data));
-            repos.forEach(function(repo) {
-                var info = extractRepo(repo);
-                addRepo(info);
+websiteApp.factory('githubService', function($http) {
+    return {
+        url: 'https://api.github.com/users/harshays/repos?sort=pushed',
+        get: function() {
+            return $http
+                .get(this.url)
+                .then(function(resp) {
+                    if (resp.statusText === "OK")
+                        return resp.data;
+                    return {};
             });
-            setup_after();
-        },
-        function(err) {
-            $node.html(gh_err);
-            setup_after();
-        });
-    }
-    catch(err) {
-        if (err instanceof ReferenceError)
-            $node.html(gh_err);
-        setup_after();
+        }
     }
 });
 
+websiteApp.filter('repoFilter', function() {
+    return function(repos) {
+        var out = [];
+
+        var repo_filter = 
+            ['harshays.gitub.io', 'website', 'hh-personal-sites', 'learn',
+             'solutions', 'scripts', 'harshays.github.io', 'dotfiles'];
+
+        var fork_filter = ['phrase-mining'];
+
+        angular.forEach(repos, function(repo) {
+            var inFilter = repo_filter.indexOf(repo['name']) > -1;
+            var inFork = fork_filter.indexOf(repo['name']) > -1;
+            var isFork = repo['fork'];
+
+            if (inFilter || (isFork && !inFork))
+                return;
+
+            out.push({
+                'name': repo['name'],
+                'url': repo['homepage'] || repo['html_url'],
+                'description': repo['description']
+            });
+        });
+        
+        return out;
+    };
+});
+
+websiteApp.controller('contentCtrl', function($scope) {
+    $scope.uiuc  = 'http://cs.illinois.edu';
+    $scope.akuna = 'http://www.akunacapital.com';
+    $scope.timan = 'http://sifaka.cs.uiuc.edu/ir/index.html';
+    $scope.email = 'hrshah4@illinois.edu';
+    $scope.github  = 'http://www.github.com/harshays';
+    $scope.linkedin = 'http://www.linkedin.com/in/harshayshah';
+    $scope.facebook = 'http://www.facebook.com/harshay.shah';
+});
+
+websiteApp.controller('projectsCtrl', function($scope, $filter, githubService) {
+    githubService.get().then(function(data) {
+        $scope.data = $filter('repoFilter')(data);
+    });
+});
