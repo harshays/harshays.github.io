a0ce72f100592ebc67ba6bf4bdf73440eb35cc5f
Harshay <harshay.rshah@gmail.com>
2017-12-28 20:22:02 +0530

    update JS

--- app.js
+++ app.js
@@ -1,20 +1,3 @@
-var websiteApp = angular.module('websiteApp', ['ngRoute']);
-
-websiteApp.service('utils', function() {
-    this.toggleActiveNav = function(route) {
-        $('.nav-li').find('a').removeClass('active-nav-li');
-        $("[nav='"+route+"']").find('a').addClass('active-nav-li');
-    }
-});
-
-websiteApp.run(function($rootScope, $location, $route, utils) {
-    $rootScope.$on('$routeChangeSuccess', function() {
-        var loc   = $location.path().substring(1);
-        var route = (loc == "") ? "about" : loc;
-        utils.toggleActiveNav(route);
-    });
-});
-
 $(document).ready(function() {
     $('body').hide().fadeIn();
 });
