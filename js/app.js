60a1b388e2265f68d25de8a3ee1eca54c6fdf79e
harshays <harshay.rshah@gmail.com>
2019-06-02 23:29:41 +0530

    add paper abstracts (blockquote, show/hide, mathjax)

--- js/app.js
+++ js/app.js
@@ -1,3 +1,10 @@
 $(document).ready(function() {
     $('body').hide().fadeIn();
+
+    $('.abstract-button').click(function() {
+      $(this).unbind("mouseenter mouseleave");
+      var id = $(this).attr('id')
+      var abs = '#'+id.replace('p', 'abs');
+      $(abs).toggle();
+    })
 });
