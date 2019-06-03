d28680183fdc728a8b53f6ef3f24574020c36b34
harshays <harshay.rshah@gmail.com>
2019-06-03 11:19:14 +0530

    fix hover issue on touch screens

--- js/app.js
+++ js/app.js
@@ -2,7 +2,6 @@ $(document).ready(function() {
     $('body').hide().fadeIn();
 
     $('.abstract-button').click(function() {
-      $(this).unbind("mouseenter mouseleave");
       var id = $(this).attr('id')
       var abs = '#'+id.replace('p', 'abs');
       $(abs).toggle();
