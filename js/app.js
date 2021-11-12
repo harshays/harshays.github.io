cb86a9d68f7bd075c1ecab81b1677f92ec9103b3
Harshay <harshay.rshah@gmail.com>
2021-11-12 15:08:37 +0530

    add bib button, summary tldr

--- js/app.js
+++ js/app.js
@@ -6,4 +6,10 @@ $(document).ready(function() {
       var abs = '#'+id.replace('p', 'abs');
       $(abs).toggle();
     })
+
+    $('.bib-button').click(function() {
+      var id = $(this).attr('id');
+      var bib = '#'+id.replace('b', 'bib');
+      $(bib).toggle();
+    })
 });
