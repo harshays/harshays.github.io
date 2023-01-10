60c628384c5f1da34cc7d8feea0a65313a7926e5
Harshay Shah <harshay.rshah@gmail.com>
2023-01-10 22:03:40 +0530

    selected

--- js/app.js
+++ js/app.js
@@ -1,15 +1,28 @@
-$(document).ready(function() {
-    $('body').hide().fadeIn();
+$(document).ready(function () {
+  $('.paper-div-ns').toggle();
+  $('body').hide().fadeIn();
+  var onlySelected = true;
 
-    $('.abstract-button').click(function() {
-      var id = $(this).attr('id')
-      var abs = '#'+id.replace('p', 'abs');
-      $(abs).toggle();
-    })
+  $('.abstract-button').click(function () {
+    var id = $(this).attr('id');
+    var abs = '#' + id.replace('p', 'abs');
+    $(abs).toggle();
+  });
 
-    $('.bib-button').click(function() {
-      var id = $(this).attr('id');
-      var bib = '#'+id.replace('b', 'bib');
-      $(bib).toggle();
-    })
+  $('.bib-button').click(function () {
+    var id = $(this).attr('id');
+    var bib = '#' + id.replace('b', 'bib');
+    $(bib).toggle();
+  });
+
+  $('#selected-button').click(function (e) {
+    if (onlySelected) {
+      e.target.innerText = 'Show selected';
+      onlySelected = false;
+    } else {
+      e.target.innerText = 'Show all';
+      onlySelected = true;
+    }
+    $('.paper-div-ns').toggle();
+  });
 });
