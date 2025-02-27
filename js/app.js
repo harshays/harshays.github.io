1c10f3569faa48242a899c88d5a68e9987ff3f0d
Harshay Shah <harshay.rshah@gmail.com>
2025-02-27 16:49:14 -0500

    minor stylistic changes, ty claude

--- js/app.js
+++ js/app.js
@@ -3,13 +3,28 @@ $(document).ready(function () {
   $('body').hide().fadeIn();
   var onlySelected = true;
 
-  $('.abstract-button').click(function () {
-    var id = $(this).attr('id');
+  // Function to toggle abstract
+  function toggleAbstract(id) {
     var abs = '#' + id.replace('p', 'abs');
     $(abs).toggle();
+  }
+
+  $('.abstract-button').click(function (e) {
+    e.stopPropagation(); // Prevent event from bubbling to paper-div
+    var id = $(this).attr('id');
+    toggleAbstract(id);
   });
 
-  $('.bib-button').click(function () {
+  // Add click handler for paper-div
+  $('.paper-div').click(function () {
+    // Find the abstract button within this paper div
+    var abstractBtn = $(this).find('.abstract-button');
+    var id = abstractBtn.attr('id');
+    toggleAbstract(id);
+  });
+
+  $('.bib-button').click(function (e) {
+    e.stopPropagation(); // Prevent event from bubbling to paper-div
     var id = $(this).attr('id');
     var bib = '#' + id.replace('b', 'bib');
     $(bib).toggle();
